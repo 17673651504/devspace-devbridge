0d08cc77ea515a2b046c0b77985566612f505905
wuhao <347143044@qq.com>
2026-09-18 19:06:10 +0800

    review

--- cli/internal/netutil/ping.go
+++ cli/internal/netutil/ping.go
@@ -1,7 +1,6 @@
 package netutil
 
 import (
-	"crypto/tls"
 	"errors"
 	"net"
 	"net/http"
@@ -19,8 +18,7 @@ func PingURI(rawURI string, timeout time.Duration) *PingResult {
 	start := time.Now()
 
 	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		Proxy:           http.ProxyFromEnvironment,
+		Proxy: http.ProxyFromEnvironment,
 	}
 	client := &http.Client{
 		Transport: transport,
